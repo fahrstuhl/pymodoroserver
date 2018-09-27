3b7eeb150360ab2eb10cfda4f291e0008f1a8340
fahrstuhl <fahrstuhl@users.noreply.github.com>
2018-09-28 00:57:37 +0200

    fixes scriptnames

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(name='pymodoroserver',
         version='1.2',
-        scripts=['pymodoroserver', 'pymodoroclient'],
+        scripts=['pymodoroserver.py', 'pymodoroclient.py'],
         author='Fahrstuhl',
         author_email='fahrstuhl@mailbox.tu-berlin.de',
         url='https://github.com/fahrstuhl/pymodoroserver',
