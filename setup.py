ebf22fcf9ea2adef25c9a06c9e6cee02d232ce7d
fahrstuhl <fahrstuhl@users.noreply.github.com>
2018-09-28 00:55:08 +0200

    changes py_modules to scripts

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(name='pymodoroserver',
-        version='1.1',
-        py_modules=['pymodoroserver', 'pymodoroclient'],
+        version='1.2',
+        scripts=['pymodoroserver', 'pymodoroclient'],
         author='Fahrstuhl',
         author_email='fahrstuhl@mailbox.tu-berlin.de',
         url='https://github.com/fahrstuhl/pymodoroserver',
